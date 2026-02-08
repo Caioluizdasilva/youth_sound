2ee219358c5fbb8ae9c20128593a0f29852deae3
Caio Luiz da Silva <caio.silva.3@aluno.cefet-rj.br>
2026-02-08 10:25:37 -0300

    update

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://songs.supabase.co";
+const SUPABASE_URL = "https://oprumuwfxxeqwigljnim.supabase.co";
 const SUPABASE_KEY = "sb_publishable_z3rvc8UI7StezvviDTAi0g_uTii1O8e";
 
 export const supabase = createClient(
